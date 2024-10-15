d8731a8b0edd49a458806ae9d45a3a55914d59a2
Mahmoud <nagboumahmoud22@gmail.com>
2024-10-15 17:10:01 +0100

    sin up working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/about";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <div className="text-red-400">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
