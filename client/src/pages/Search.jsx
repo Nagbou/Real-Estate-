e9408d041d7da3578f1556fc3400127d4e17e424
Mahmoud <nagboumahmoud22@gmail.com>
2024-10-30 18:57:23 +0100

    UI finished

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ListingItem from "../components/ListingItem";
+import { FaSearch } from "react-icons/fa";
+import styles from "./Search.module.css";
 export default function Search() {
   const navigate = useNavigate();
   const [sidebardata, setSidebardata] = useState({
@@ -115,110 +117,106 @@ export default function Search() {
   };
   return (
     <div className="flex flex-col md:flex-row">
-      <div className="p-7  border-b-2 md:border-r-2 md:min-h-screen">
+      <div className="p-7  border-b-2 md:border-r-2 md:min-h-screen bg-[#0F4C5C]">
         <form onSubmit={handleSubmit} className="flex flex-col gap-8">
-          <div className="flex items-center gap-2">
-            <label className="whitespace-nowrap font-semibold">
-              Search Term:
-            </label>
+          <div className={styles.searchForm}>
             <input
               type="text"
               id="searchTerm"
               placeholder="Search..."
-              className="border rounded-lg p-3 w-full"
+              className={styles.searchInput}
               value={sidebardata.searchTerm}
               onChange={handleChange}
             />
+            <button className={styles.searchIcon}>
+              <FaSearch />
+            </button>
           </div>
-          <div className="flex gap-2 flex-wrap items-center">
-            <label className="font-semibold">Type:</label>
-            <div className="flex gap-2">
+          <div className="space-y-3 ms-6">
+            <div className="flex items-center gap-2 mb-7">
+              <select
+                onChange={handleChange}
+                defaultValue={"created_at_desc"}
+                id="sort_order"
+                className="border border-yellow-600 rounded-lg p-3 pe-4"
+              >
+                <option value="createdAt_desc">Latest</option>
+                <option value="createdAt_asc">Oldest</option>
+                <option value="regularPrice_desc">Price high to low</option>
+                <option value="regularPrice_asc">Price low to hight</option>
+              </select>
+            </div>
+            <div className={`${styles.vertical_yellow_box} text-white `}>
               <input
                 type="checkbox"
                 id="all"
-                className="w-5"
+                className="w-5 h-5"
                 onChange={handleChange}
                 checked={sidebardata.type === "all"}
               />
-              <span>Rent & Sale</span>
+              <span className="ms-2">Rent & Sale</span>
             </div>
-            <div className="flex gap-2">
+            <div className={`${styles.vertical_yellow_box} text-white `}>
               <input
                 type="checkbox"
                 id="rent"
-                className="w-5"
+                className="w-5 h-5"
                 onChange={handleChange}
                 checked={sidebardata.type === "rent"}
               />
-              <span>Rent</span>
+              <span className="ms-2">Rent</span>
             </div>
-            <div className="flex gap-2">
+            <div className={`${styles.vertical_yellow_box} text-white `}>
               <input
                 type="checkbox"
                 id="sale"
-                className="w-5"
+                className="w-5 h-5"
                 onChange={handleChange}
                 checked={sidebardata.type === "sale"}
               />
-              <span>Sale</span>
+              <span className="ms-2">Sale</span>
             </div>
-            <div className="flex gap-2">
+            <div className={`${styles.vertical_yellow_box} text-white `}>
               <input
                 type="checkbox"
                 id="offer"
-                className="w-5"
+                className="w-5 h-5"
                 onChange={handleChange}
                 checked={sidebardata.offer}
               />
-              <span>Offer</span>
+              <span className="ms-2">Offer</span>
             </div>
-          </div>
-          <div className="flex gap-2 flex-wrap items-center">
-            <label className="font-semibold">Amenities:</label>
-            <div className="flex gap-2">
+            <div className={`${styles.vertical_yellow_box} text-white `}>
               <input
                 type="checkbox"
                 id="parking"
-                className="w-5"
+                className="w-5 h-5"
                 onChange={handleChange}
                 checked={sidebardata.parking}
               />
-              <span>Parking</span>
+              <span className="ms-2">Parking</span>
             </div>
-            <div className="flex gap-2">
+            <div className={`${styles.vertical_yellow_box} text-white `}>
               <input
                 type="checkbox"
                 id="furnished"
-                className="w-5"
+                className="w-5 h-5"
                 onChange={handleChange}
                 checked={sidebardata.furnished}
               />
-              <span>Furnished</span>
+              <span className="ms-2">Furnished</span>
             </div>
           </div>
-          <div className="flex items-center gap-2">
-            <label className="font-semibold">Sort:</label>
-            <select
-              onChange={handleChange}
-              defaultValue={"created_at_desc"}
-              id="sort_order"
-              className="border rounded-lg p-3"
-            >
-              <option value="regularPrice_desc">Price high to low</option>
-              <option value="regularPrice_asc">Price low to hight</option>
-              <option value="createdAt_desc">Latest</option>
-              <option value="createdAt_asc">Oldest</option>
-            </select>
-          </div>
-          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
+
+          <button className="bg-[#1A1A2E] text-white p-3 rounded-lg uppercase hover:opacity-95">
             Search
           </button>
         </form>
       </div>
       <div className="flex-1">
-        <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
-          Listing results:
-        </h1>
+        <div className="bg-[#1A1A2E]  font-bold p-6 mb-4 text-white text-3xl ">
+          <span className={styles.vertical_yellow_box}> Listing results:</span>
+        </div>
         <div className="p-7 flex flex-wrap gap-4">
           {!loading && listings.length === 0 && (
             <p className="text-xl text-slate-700">No listing found!</p>
